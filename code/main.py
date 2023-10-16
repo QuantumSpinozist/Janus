5fe84a90ab7c2c562150c6115352838834e184b4
Akkusativ23 <s6arweis@uni-bonn.de>
2023-10-16 08:56:48 +0200

    added WatchWidget class and simple timer label

--- code/main.py
+++ code/main.py
@@ -1,12 +1,27 @@
-
 from tkinter import *
 from ttkbootstrap.constants import *
 import ttkbootstrap as tb
-from watch import Watch
+from watch import Watch, WatchWidget
+import time
+
+study_watch = Watch('study')
 
 root = tb.Window(themename='superhero')
+
+
+
 root.title('Janus')
 root.geometry('500x350')
 
+study_widget = WatchWidget(root, study_watch)
+study_widget.watch.start()
+study_widget.counter_label()
+
+
+study_widget.display()
+
+
+
 root.mainloop()
+
 
--- code/watch.py
+++ code/watch.py
@@ -1,5 +1,9 @@
+from tkinter import *
+from ttkbootstrap.constants import *
+import ttkbootstrap as tb
 import threading
 import time
+import datetime
 
 class Watch:
     def __init__(self, name):
@@ -26,13 +30,35 @@ class Watch:
             time.sleep(1)
             self._elapsed_time = time.time() - self._start_time
 
-    def get_time(self):
-        return self._elapsed_time
+    def get_time(self, as_str = False):
+        if as_str:
+            seconds = int(self._elapsed_time)
+            return str(datetime.timedelta(seconds=seconds))
+        else:
+            return self._elapsed_time
+    
+    def is_running(self):
+        return self._running
     
 
 class WatchWidget:
-    def __init__(self, watch):
+    def __init__(self, root,watch):
+        self.root = root
         self.watch = watch
+        self.label = tb.Label(self.root, text='Test',bootstyle="default")
+
+    def counter_label(self):
+        def counter():
+            if self.watch.is_running():
+                self.label['text'] = self.watch.get_time(as_str=True)
+                self.label.after(1000, counter)
+        counter()
+
+    def display(self):
+        self.label.pack(pady=50)
+
+
+
 
 
 
